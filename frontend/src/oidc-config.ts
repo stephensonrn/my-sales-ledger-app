a20f0ca118bd14bcf2d47eff3f733bdab4db79f7
Ross Stephenson <ross@aurumif.com>
2025-06-18 17:18:33 +0100

    oidc

--- frontend/src/oidc-config.ts
+++ frontend/src/oidc-config.ts
@@ -1,6 +1,6 @@
 // src/oidc-config.ts
 export const oidcConfig = {
-  authority: 'https://auth.salesledgersync.com', // your Cognito domain
+  authority: 'https://eu-west-1i09ij2ysb.auth.eu-west-1.amazoncognito.com', // your Cognito domain
   client_id: '28889re05prqhvu9kr7g5jtdid',
   redirect_uri: 'https://www.salesledgersync.com', // or http://localhost:5173
   post_logout_redirect_uri: 'https://www.salesledgersync.com',
