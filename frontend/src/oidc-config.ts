198a01f0c5ce09fccfd6cc3cad0510f64e8807c3
Ross Stephenson <ross@aurumif.com>
2025-06-19 16:06:45 +0100

    oidc

--- frontend/src/oidc-config.ts
+++ frontend/src/oidc-config.ts
@@ -1,5 +1,5 @@
 export const oidcConfig = {
-  authority: 'https://auth.salesledgersync.com',
+  authority: 'https://eu-west-1i09ij2ysb.auth.eu-west-1.amazoncognito.com',
   client_id: '28889re05prqhvu9kr7g5jtdid',
   redirect_uri: 'https://www.salesledgersync.com',
   post_logout_redirect_uri: 'https://www.salesledgersync.com',
